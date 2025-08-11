c761660cc120a77adee942b3c4a3dfa6bac3f01c
Your Name <you@example.com>
2025-08-11 15:05:28 +0530

    feat: Home component done

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,37 @@
+import { BiLogoGithub, BiLogoLinkedin } from "react-icons/bi"
+import image from "../../public/user.png"
+import type { IconType } from "react-icons"
 
-type Props = {}
 
-const Home = (props: Props) => {
+const Home = () => {
+  const name: string = "Prince Patel"
+  const role: string = "Software Developer"
+  const description: string = "description"
+  const Links: Record<string, IconType> = {
+    "https://github.com/Prince2412k2": BiLogoGithub,
+    "https://www.linkedin.com/in/prince2412k2": BiLogoLinkedin,
+  };
   return (
-    <div>Home</div>
+    <div id="home" className="flex min-h-screen w-full items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-8 p-5 text-center">
+        <img src={image} alt="user" className="w-[250px] sm:w-[300px] rounded-full" />
+        <div className="space-y-1 sm:space-y-3">
+          <h1 className="bg-gradient-to-r from-teal-400 to-teal-600 bg-clip-text text-4xl font-semibold text-transparent md:text-5x1 lg:text-6xl">{name}</h1>
+          <h3 className="bg-gradient-to-r from-teal-400 to-teal-600 bg-clip-text text-xl font-semibold text-transparent md:text-2x1 lg:text-3xl">{role}</h3>
+          <p className="max-w-[500] text-sm text-gray-500">{description}</p>
+        </div>
+        <div className="flex gap-3">
+
+          {Object.entries(Links).map(([link, Icon]) => (
+            <a href={link} target="_blank" rel="noopener noreferrer" className="flex flex-col items-center justify-center" >
+              <Icon className="h-10 w-10 
+            cursor-pointer rounded-full border-2 border-transparent bg-teal-600 p-2 text-white transition-all duration-200 
+            hover:scale-110 hover:border-teal-600 hover:bg-white hover:text-teal-600 md:h-12 md:w-12"/>
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
